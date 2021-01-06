a9650372f669d2a519cdd6078dfe8f5c355d2542
Chi Nguyen <c.nguyen.prog@gmail.com>
2021-01-06 17:09:54 +0100

    add albums viewer

--- src/components/AlbumsManager.vue
+++ src/components/AlbumsManager.vue
@@ -1,26 +1,61 @@
 <template>
   <div class="h-full w-full flex flex-row flex-wrap">
     <div v-for="album of albums" :key="album.id">
-      <div class="relative h-40 w-40 m-2">
+      <div
+        class="album relative h-40 w-40 m-2 cursor-pointer rounded"
+        @click="selectAlbum(album)"
+        :class="{ 'selected border-4 border-blue-400': selected.includes(album) }"
+      >
         <img :src="album.coverPhotoBaseUrl" alt="" class="absolute h-full object-cover rounded" />
       </div>
+      <div class="album-info text-sm text-center">
+        <p class="">{{ album.title }}</p>
+        <p class="text-sm">{{ album.mediaItemsCount }} {{ $tc('album.photo', album.mediaItemsCount) }}</p>
+      </div>
     </div>
+
+    <button
+      class="fixed right-0 bottom-0 bg-blue-400 hover:bg-blue-500 focus:outline-none px-6 py-2 text-base font-bold mr-8 mb-8 rounded-full appearance-none outline-none"
+      @click="syncSelectedAlbums()"
+    >
+      Sync
+    </button>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Watch } from 'vue-property-decorator'
 import { vxm } from '@/store/store.vuex'
+// eslint-disable-next-line no-undef
+import Album = gapi.client.photoslibrary.Album
 
-@Component({})
+@Component
 export default class AlbumsManager extends Vue {
+  selected: Album[] = []
+
   created() {
     this.$store.dispatch('photos/listAlbums')
   }
+
   @Watch('isSignedIn')
   onIsSignedInChanged() {
     this.$store.dispatch('photos/listAlbums')
   }
+
+  selectAlbum(album: Album) {
+    if (!this.selected.includes(album)) {
+      this.selected.push(album)
+    } else {
+      const index = this.selected.indexOf(album)
+      if (index > -1) {
+        this.selected.splice(index, 1)
+      }
+    }
+  }
+  syncSelectedAlbums() {
+    console.log('sync')
+  }
+
   get isSignedIn() {
     return vxm.photos.isSignedIn
   }
@@ -32,4 +67,13 @@ export default class AlbumsManager extends Vue {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style scoped>
+.album {
+  transform: scale(1);
+  transition: all 0.1s ease-in-out;
+}
+.selected {
+  transform: scale(0.9);
+  transition: all 0.1s ease-in-out;
+}
+</style>
--- src/pages/PhotosManager.vue
+++ src/pages/PhotosManager.vue
@@ -2,7 +2,7 @@
   <div
     class="relative min-h-screen text-3xl pt-20 md:pt-4 p-4 pl-4 md:pl-48 text-white flex flex-col items-center text-justify"
   >
-    Photos Manager
+    <span class="pb-4">Photos Manager</span>
     <GooglePhotosAuth v-if="!isSignedIn" />
     <AlbumsManager v-else />
   </div>
